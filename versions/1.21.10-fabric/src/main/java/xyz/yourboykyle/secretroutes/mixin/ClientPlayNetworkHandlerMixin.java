9142748ef77c17fdc84d8f59c8401538f82f933b
Chrisitian Steenkamp <christiannsteenkamp@gmail.com>
2026-01-24 16:52:52 +0200

    Sound is now handled via packets.

--- versions/1.21.10-fabric/src/main/java/xyz/yourboykyle/secretroutes/mixin/ClientPlayNetworkHandlerMixin.java
+++ versions/1.21.10-fabric/src/main/java/xyz/yourboykyle/secretroutes/mixin/ClientPlayNetworkHandlerMixin.java
@@ -3,11 +3,13 @@ package xyz.yourboykyle.secretroutes.mixin;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ItemPickupAnimationS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.yourboykyle.secretroutes.events.OnReceivePacket;
+import xyz.yourboykyle.secretroutes.events.recording.RecordPlaySound;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
@@ -20,5 +22,10 @@ public class ClientPlayNetworkHandlerMixin {
     @Inject(method = "onBlockUpdate", at = @At("HEAD"))
     private void onBlockUpdate(BlockUpdateS2CPacket packet, CallbackInfo ci) {
         OnReceivePacket.onBlockUpdate(packet);
+    }
+
+    @Inject(method = "onPlaySound", at = @At("HEAD"))
+    private void onPlaySoundPacket(PlaySoundS2CPacket packet, CallbackInfo ci) {
+        RecordPlaySound.handleSoundPlayed(packet);
     }
 }
